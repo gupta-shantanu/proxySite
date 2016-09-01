14d4a5d0b3974649483301be694bf01ba553bca3
gupta-shantanu <shantanu@programmer.net>
2016-09-02 02:11:39 +0530

    cookie is buggy!

--- url/views.py
+++ url/views.py
@@ -1,42 +1,59 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-import requests
+from django.views.decorators.csrf import csrf_exempt
+from http import cookies,cookiejar
 from urllib import request as req
 from bs4 import BeautifulSoup as bs
-from io import BytesIO
-import base64
 
+
+@csrf_exempt
 def viewurl(request,url):
-    if url[-4:]==".ext":
-        return HttpResponse(requests.get(url[:-4]).text)
-    html=requests.get(url)
-    pos=url.find('?')
+
+
+
+
+    content=req.urlopen(req.Request(url,headers={'Cookie': ("" if not 'cookie' in request.COOKIES else request.COOKIES['cookie'])}))
+    html=content.read()
+    cook=cookies.SimpleCookie()
+    if not 'html' in content.getheader('Content-Type'):
+        #startswith('text/html')
+        response=HttpResponse(html, content_type=content.getheader('Content-Type'))
+        # for key,value in content.getheaders():
+        #     if not key in hop:
+        #      response[key]=content.getheader(key)
+        # #response['Set-Cookie']=content.getheader('Set-Cookie')
+        if 'cookie' in request.COOKIES:
+            response.set_cookie( 'cookie', request.COOKIES['cookie']+str(content.getheader('Set-Cookie')))
+        else:
+            response.set_cookie( 'cookie', content.getheader('Set-Cookie'))
+
+
+        return response
+
+    pos=url.find('/',9)
     if pos>-1:
         url=url[:pos]
-    soup=bs(html.text,"html.parser")
+    soup=bs(html,"html.parser")
     replacelinks(soup.find_all('form', attrs = {'action' : True}),url,'action')
     replacelinks(soup.find_all('script', attrs = {'src' : True}),url,'src')
 
     replacelinks(soup.find_all('link', attrs = {'href' : True}),url)
     replacelinks(soup.find_all('a', attrs = {'href' : True}),url)
+    replacelinks(soup.find_all('img', attrs = {'src' : True}),url,'src')
 
-    result=soup.find_all('img', attrs = {'src' : True})
-    for i in result:
-
-            makeabsolute(i,url,'src')
-            try:
-                img=req.urlopen(i['src'])
-                imag=base64.b64encode(BytesIO(img.read()).getvalue()).decode()
-                i['src']="data:image/%s;base64,"%(i['src'][-3:])+imag
-            except:
-                i['src']=""
-
-            if i.has_attr('srcset'):
-                del i['srcset']
+    response=HttpResponse(str(soup)+str(request.COOKIES))
+    if 'cookie' in request.COOKIES:
+        response.set_cookie( 'cookie', request.COOKIES['cookie']+str(content.getheader('Set-Cookie')))
+    else:
+        response.set_cookie( 'cookie', content.getheader('Set-Cookie'))
 
 
-    return HttpResponse(str(soup))
+    # for key,value in content.getheaders():
+    #     if not key in hop:
+    #          response[key]=content.getheader(key)
+    # #response['Set-Cookie']=content.getheader('Set-Cookie')
+
+    return response
 
 def getform(request):
         return render(request,'form.html')
@@ -52,11 +69,13 @@ def replacelinks(result,url,param='href'):
     for i in result:
         try:
             makeabsolute(i,url,param)
-            if not i[param].endswith('png'):
-                i[param]='/'+i[param]
-                if i.has_attr('type') or i.has_attr('rel') or i.has_attr('src'):
-                    i[param]=i[param]+".ext"
-            else:
-                i[param]='https://willnorris.com/api/imageproxy/'+i[param]
+
+            i[param]='/'+i[param]
+            if param =='action':
+                new_tag = soup.new_tag("a", href="http://www.example.com")
+                original_tag.append(new_tag)
+
+
+
         except:
             pass
